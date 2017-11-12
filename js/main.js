babb641b55e74fc9cb581eec0925a5e575875b99
ZiluoH <ziluoh@gmail.com>
2017-11-11 22:54:54 -0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -58,3 +58,48 @@ query.once('value')
 			// console.log(childData)
 		})
 	})
+
+
+
+// JS for frontend
+function addItem() {
+	var fff = document.querySelector('input[name="size"]:checked').value;
+	console.log(fff)
+}
+
+// function for topping
+var toppingArray = []
+function topping(type,price) {
+	this.type = type
+	this.price = price
+	toppingArray.push(this)
+}
+
+var boba = new topping('Boba', 0)
+var grassjelly = new topping('Grass Jelly', 0.5)
+var rainbowjelly = new topping('Rainbow Jelly', 0.5)
+var lycheejelly = new topping('Lychee Jelly', 0.5)
+var greenapplejelly = new topping('Greenapple Jelly', 0.5)
+var mangojelly = new topping('Mango Jelly', 0.5)
+var pineapplejelly = new topping('Pineapple Jelly', 0.5)
+var basilseeds = new topping('Basil Seeds', 0.5)
+var eggpudding = new topping('Egg Pudding', 0.5)
+var aloevera = new topping('Aloe Vera', 0.5)
+var redbean = new topping('Red Bean', 0.5)
+var poppingboba = new topping('Popping Boba(lychee)', 0.5)
+
+for (var i = toppingArray.length - 1; i >= 0; i--) {
+	var toppingLabel = document.createElement('label')
+	var toppingInput = document.createElement('input')
+
+	toppingLabel.className = 'btn btn-primary'
+  toppingLabel.innerHTML = toppingArray[i].type
+
+	toppingInput.type = 'checkbox'
+	toppingInput.name = 'topping'
+	toppingInput.value = toppingArray[i].type
+
+	toppingLabel.appendChild(toppingInput)
+
+	document.getElementById('make_drinks_topping').appendChild(toppingLabel)
+}
